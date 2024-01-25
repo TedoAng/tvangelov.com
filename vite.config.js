846f5aba2022099c75dba174fa34946abd4de548
Teodor Angelov <teodor.angelov@fastcomet.com>
2024-01-25 16:57:51 +0200

    site is now PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()]
+  plugins: [
+    vue(), 
+    svgLoader(),
+    VitePWA({
+      manifest: {
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        workbox: {
+          clientsClaim: true,
+          skipWaiting: true
+        },
+        icons: [
+          {
+            src: "./assets/images/icon512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ]
 })
